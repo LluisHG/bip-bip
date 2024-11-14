48aca918152289bcf2d3365215fb194be4684851
lluis <lluis.honte@gmail.com>
2024-11-14 20:58:27 +0100

    Code refatored and first readme version created

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # bip-bip
-Code and other information about the distance sensor designed to use in a car
+
+Bip-bip is an Arduino Nano based car parking sensor project. In this repository is included the arduino code and the hardware used as well as the 3D printed parts to assemble the sensor.
+
+
+## Hardware
+The selected micro controller is an Arduino Nano board, concretely the [Arduino Nano 3,0 Atmega328](https://es.aliexpress.com/item/1005004305055818.html?spm=a2g0o.order_list.order_list_main.11.3af3194dyjIC2R&gatewayAdapt=glo2esp).
+
+The ultrasonic sensors used are the waterprof ultrasonic sensor [SR04M-2](doc:JSN-SR04T-2.0.pdf).
+
+The device to produce the signal to tell the user when the car is getting closer to an obstacle will be an active buzzer.
+
+Below, you can find the schematics of the whole system. 
+
+[Schematics]
+
+## Software
+
+Run the Arduino GUI to open the [main file](bip-bip-sensor/bip-bip-sensor.ino). 
+At the beginning of the program, you can see the Arduino Nano PIN initialisations, the number of sensor used and the saefty distance defined:
+
+```c++
+int sensor_01_ECHO_PIN = 2;
+int sensor_01_TRIG_PIN = 3;
+int sensor_02_ECHO_PIN = 4;
+int sensor_02_TRIG_PIN = 5;
+int sensor_03_ECHO_PIN = 6;
+int sensor_03_TRIG_PIN = 7;
+int sensor_04_ECHO_PIN = 8;
+int sensor_04_TRIG_PIN = 9;
+int BUZZER_PIN = 10;
+int LED_PIN = 12;
+int NUMBER_OF_SENSORS = 2;
+int SAFETY_DISTANCE = 50; // Centimeters
+int BAUD_RATE = 9600;
+```
+
+Note that the `SAEFTY_DISTANCE` is in centimetres and is the farest distance the system starts bip-biping.
+
+Pay attention at the method [measureDistance](https://vscode.dev/github/LluisHG/bip-bip/blob/feature-code-refactor/bip-bip-sensor/bip-bip-sensor.ino#L55). Note that there are some commented code lines. Those lines should be necessary if the sensor worked as expected. In my case and in my configuration, it was only necessary to read the signal at the `ECHO` pin and calculate the distance.
+
+```c++
+void measureDistance(Sensor &sensor) {
+  // digitalWrite(sensor.trig_pin, LOW);
+  // delayMicroseconds(2);
+  // digitalWrite(sensor.trig_pin, HIGH);
+  // delayMicroseconds(10);
+  // digitalWrite(sensor.trig_pin, LOW);
+
+  unsigned long duration = pulseIn(sensor.echo_pin, HIGH);
+  if (duration == 0) {
+    Serial.println("Warning: no pulse from sensor");
+    sensor.measured_distance_cm = -1; // Indica distancia inválida
+  } else {
+    sensor.measured_distance_cm = duration / 29 / 2;
+  }
+}
+```
+
+Read the sensor documentation to understand its working modes.
+
+## Usage
+
--- bip-bip-sensor/bip-bip-sensor.ino
+++ bip-bip-sensor/bip-bip-sensor.ino
@@ -1,119 +1,22 @@
-// #define sensor_01_ECHO_PIN 2
-// #define sensor_01_TRIG_PIN 3
-// // #define sensor_02_ECHO_PIN 4
-// // #define sensor_02_TRIG_PIN 5
-// // #define sensor_03_ECHO_PIN 6
-// // #define sensor_03_TRIG_PIN 7
-// // #define sensor_04_ECHO_PIN 8
-// // #define sensor_04_TRIG_PIN 9
-// #define BUZZER_PIN 10
-// #define LED_PIN 12
-
-// #define NUMBER_OF_SENSORS 1
-    
-// struct sensor_data {
-//   int echo_pin;
-//   int trig_pin;
-//   int measured_distance_cm;
-// };
-
-// struct sensor_data sensor[NUMBER_OF_SENSORS];
-
-// const unsigned int BAUD_RATE = 9600;
-// const unsigned int SAEFTY_DISTANCE = 100;
-
-// void setup() {
-
-//   sensor[0].echo_pin = sensor_01_ECHO_PIN;
-//   sensor[0].trig_pin = sensor_01_TRIG_PIN;
-//   // sensor[1].echo_pin = sensor_02_ECHO_PIN;
-//   // sensor[1].trig_pin = sensor_02_TRIG_PIN;
-//   // sensor[2].echo_pin = sensor_03_ECHO_PIN;
-//   // sensor[2].trig_pin = sensor_03_TRIG_PIN;
-//   // sensor[3].echo_pin = sensor_04_ECHO_PIN;
-//   // sensor[3].trig_pin = sensor_04_TRIG_PIN;
-
-//   pinMode(sensor[0].trig_pin, OUTPUT);
-//   pinMode(sensor[0].echo_pin, INPUT);
-//   // pinMode(sensor[1].trig_pin, OUTPUT);
-//   // pinMode(sensor[1].echo_pin, INPUT);
-//   // pinMode(sensor[2].trig_pin, OUTPUT);
-//   // pinMode(sensor[2].echo_pin, INPUT);
-//   // pinMode(sensor[3].trig_pin, OUTPUT);
-//   // pinMode(sensor[3].echo_pin, INPUT);
-
-//   // pinMode(TRIG_PIN, OUTPUT);
-//   // pinMode(ECHO_PIN, INPUT);
-//   pinMode(BUZZER_PIN, OUTPUT);
-//   pinMode(LED_PIN, OUTPUT);
-//   Serial.begin(BAUD_RATE);
-// }
-
-// void loop() {
-
-//   for (int i = 0; i < NUMBER_OF_SENSORS; i++)
-//   {
-//     digitalWrite(sensor[i].trig_pin, LOW);
-//     delayMicroseconds(2);
-//     digitalWrite(sensor[i].trig_pin, HIGH);
-//     delayMicroseconds(10);
-//     digitalWrite(sensor[i].trig_pin, LOW);  
-
-//     const unsigned long duration = pulseIn(sensor[i].echo_pin, HIGH);
-//     sensor[i].measured_distance_cm = duration/29/2;
-//     int distance = sensor[i].measured_distance_cm;
-
-//     if(duration == 0) {
-//       Serial.println("Warning: no pulse from sensor");
-//     } else {
-//       if (distance <= SAEFTY_DISTANCE) {
-        
-//         long tone_duration = round(500 * (1 - distance /SAEFTY_DISTANCE));
-//         tone(BUZZER_PIN, 2500, tone_duration);
-//         // delay(tone_duration);
-//         // tone(BUZZER_PIN, 2500, tone_duration);
-
-
-//         digitalWrite(LED_PIN, HIGH);
-//       } else {
-//         noTone(BUZZER_PIN);
-//         digitalWrite(LED_PIN, LOW);
-//       }
-      
-//       Serial.print("Sensor ");
-//       Serial.print(i);
-//       Serial.print(" distance to nearest object: ");
-//       Serial.print(distance);
-//       Serial.println(" cm");
-//     }
-//   }
-
-  
-//   delay(500);
-// }
-
-#define sensor_01_ECHO_PIN 2
-#define sensor_01_TRIG_PIN 3
-#define sensor_02_ECHO_PIN 4
-#define sensor_02_TRIG_PIN 5
-#define sensor_03_ECHO_PIN 6
-#define sensor_03_TRIG_PIN 7
-#define sensor_04_ECHO_PIN 8
-#define sensor_04_TRIG_PIN 9
-#define BUZZER_PIN 10
-#define LED_PIN 12
-
-#define NUMBER_OF_SENSORS 2
-#define SAFETY_DISTANCE 50 // Centimeters
-// #define MAX_TONE_DURATION 500 // Milliseconds
-// #define BUZZER_FREQUENCY 2500 // Hertz
-#define BAUD_RATE = 9600;
-
 #define arrSize(x) sizeof(x) / sizeof(x[0])
+
+int sensor_01_ECHO_PIN = 2;
+int sensor_01_TRIG_PIN = 3;
+int sensor_02_ECHO_PIN = 4;
+int sensor_02_TRIG_PIN = 5;
+int sensor_03_ECHO_PIN = 6;
+int sensor_03_TRIG_PIN = 7;
+int sensor_04_ECHO_PIN = 8;
+int sensor_04_TRIG_PIN = 9;
+int BUZZER_PIN = 10;
+int LED_PIN = 12;
+int NUMBER_OF_SENSORS = 2;
+int SAFETY_DISTANCE = 50; // Centimeters
+int BAUD_RATE = 9600;
 
 struct Sensor {
   int echo_pin;
-  // int trig_pin;
+  int trig_pin;
   long measured_distance_cm;
 };
 
@@ -141,8 +44,8 @@ void loop() {
 void initializeSensors() {
   for (int i = 0; i < NUMBER_OF_SENSORS; i++) {
     sensors[i].echo_pin = sensor_01_ECHO_PIN + i * 2;
-    // sensors[i].trig_pin = sensor_01_TRIG_PIN + i * 2;
-    // pinMode(sensors[i].trig_pin, OUTPUT);
+    sensors[i].trig_pin = sensor_01_TRIG_PIN + i * 2;
+    pinMode(sensors[i].trig_pin, OUTPUT);
     pinMode(sensors[i].echo_pin, INPUT);
   }
   pinMode(BUZZER_PIN, OUTPUT);
@@ -170,7 +73,7 @@ void handleAlarm(long measured_distance_cm) {
   int tone_duration;
   if (measured_distance_cm >= 10 && measured_distance_cm <= SAFETY_DISTANCE) {
     tone_duration = map(measured_distance_cm, 10, SAFETY_DISTANCE, 20, 500); // Ajusta los valores según sea necesario
-    // Hace sonar el buzzer
+    // Plays the buzzer
     digitalWrite(BUZZER_PIN, HIGH);
     delay(50); // Duración del pitido
     digitalWrite(BUZZER_PIN, LOW);
