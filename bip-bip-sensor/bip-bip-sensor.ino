fd573bd50bfbeeb5202c683213d306045cafeb35
lluis <lluis.honte@gmail.com>
2024-12-15 22:31:24 +0100

    New buzzer added at DO11; lead always blinking at loop's frequency

--- bip-bip-sensor/bip-bip-sensor.ino
+++ bip-bip-sensor/bip-bip-sensor.ino
@@ -9,6 +9,7 @@ int sensor_03_TRIG_PIN = 7;
 int sensor_04_ECHO_PIN = 8;
 int sensor_04_TRIG_PIN = 9;
 int BUZZER_PIN = 10;
+int BUZZER_2_PIN = 11;
 int LED_PIN = 12;
 const int NUMBER_OF_SENSORS = 4;
 int SAFETY_DISTANCE = 50; // Centimeters
@@ -30,6 +31,8 @@ void setup() {
 
 void loop() {
 
+  digitalWrite(LED_PIN, HIGH);
+
   for (int i = 0; i < NUMBER_OF_SENSORS; i++) {
     measureDistance(sensors[i]);
     distances_read[i] = sensors[i].measured_distance_cm;
@@ -45,10 +48,11 @@ void initializeSensors() {
   for (int i = 0; i < NUMBER_OF_SENSORS; i++) {
     sensors[i].echo_pin = sensor_01_ECHO_PIN + i * 2;
     sensors[i].trig_pin = sensor_01_TRIG_PIN + i * 2;
-    pinMode(sensors[i].trig_pin, OUTPUT);
+    //pinMode(sensors[i].trig_pin, OUTPUT);
     pinMode(sensors[i].echo_pin, INPUT);
   }
   pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(BUZZER_2_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 }
 
@@ -72,14 +76,22 @@ void handleAlarm(long measured_distance_cm) {
   
   int tone_duration;
   if (measured_distance_cm >= 10 && measured_distance_cm <= SAFETY_DISTANCE) {
-    tone_duration = map(measured_distance_cm, 10, SAFETY_DISTANCE, 20, 500); // Ajusta los valores según sea necesario
-    // Plays the buzzer
+    
+    tone_duration = map(measured_distance_cm, 10, SAFETY_DISTANCE, 10, 500); // Ajusta los valores según sea necesario
+    
+    // Plays the first buzzer
     digitalWrite(BUZZER_PIN, HIGH);
+    
+    // Second buzzer
+    digitalWrite(BUZZER_2_PIN, HIGH);
+
     delay(50); // Duración del pitido
     digitalWrite(BUZZER_PIN, LOW);
-    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(BUZZER_2_PIN, LOW);
+
 	  delay(tone_duration);
-  } else {
+  }
+  else {
     digitalWrite(LED_PIN, LOW);
   }
 }
